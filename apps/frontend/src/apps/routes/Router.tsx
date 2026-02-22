367240b2c4155bdf208905d6b6760ab4759bd5c3
Dobbymin <gmin990624@gmail.com>
2026-02-22 11:14:28 +0900

    feat: router 설정 추가

--- apps/frontend/src/apps/routes/Router.tsx
+++ apps/frontend/src/apps/routes/Router.tsx
@@ -1,6 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 
-import { MainPage } from "@/pages";
+import {
+  DockerPage,
+  FilesPage,
+  LoginPage,
+  MainPage,
+  NetworkPage,
+  ServicesPage,
+  SettingsPage,
+  StoragePage,
+  SystemPage,
+  TerminalPage,
+  UsersPage,
+} from "@/pages";
 import { ROUTE_PATHS } from "@/shared";
 import { RootLayout } from "@/widgets";
 
@@ -8,8 +20,21 @@ export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Public */}
+        <Route path={ROUTE_PATHS.LOGIN} element={<LoginPage />} />
+
+        {/* Protected (with Layout) */}
         <Route element={<RootLayout />}>
           <Route path={ROUTE_PATHS.MAIN} element={<MainPage />} />
+          <Route path={ROUTE_PATHS.SYSTEM} element={<SystemPage />} />
+          <Route path={ROUTE_PATHS.SERVICES} element={<ServicesPage />} />
+          <Route path={ROUTE_PATHS.NETWORK} element={<NetworkPage />} />
+          <Route path={ROUTE_PATHS.STORAGE} element={<StoragePage />} />
+          <Route path={ROUTE_PATHS.FILES} element={<FilesPage />} />
+          <Route path={ROUTE_PATHS.USERS} element={<UsersPage />} />
+          <Route path={ROUTE_PATHS.TERMINAL} element={<TerminalPage />} />
+          <Route path={ROUTE_PATHS.DOCKER} element={<DockerPage />} />
+          <Route path={ROUTE_PATHS.SETTINGS} element={<SettingsPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
