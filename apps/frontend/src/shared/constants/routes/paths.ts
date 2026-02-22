9c241c859e7699f7993cb98df6662262b90db55e
Dobbymin <gmin990624@gmail.com>
2026-02-22 11:27:05 +0900

    feat: router path 상수화

--- apps/frontend/src/shared/constants/routes/paths.ts
+++ apps/frontend/src/shared/constants/routes/paths.ts
@@ -2,5 +2,13 @@ export const ROUTE_PATHS = {
   MAIN: "/",
   LOGIN: "/login",
   SIGN_UP: "/signup",
+  SYSTEM: "/system",
+  SERVICES: "/services",
+  NETWORK: "/network",
+  STORAGE: "/storage",
+  FILES: "/files",
+  USERS: "/users",
+  TERMINAL: "/terminal",
+  DOCKER: "/docker",
   SETTINGS: "/settings",
 };
